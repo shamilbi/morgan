ae1592e3f9ca049691e6af8a934a2720e1896ed4
shamilbi <shamilbi@gmail.com>
2025-11-22 18:49:52 +0200

    +RequestCache

--- morgan/utils.py
+++ morgan/utils.py
@@ -1,5 +1,8 @@
+import json
 import os
 import re
+import urllib.parse
+import urllib.request
 
 import dateutil  # type: ignore[import-untyped]
 from packaging.requirements import Requirement
@@ -54,3 +57,31 @@ def touch_file(path: str, fileinfo: dict):
     dt = dateutil.parser.parse(time_str)
     ts = dt.timestamp()
     os.utime(path, (ts, ts))
+
+
+class RequestCache:  # pylint: disable=too-few-public-methods
+    d: dict[str, dict] = {}  # name: data
+
+    def get(self, url: str, name: str) -> dict:
+        if name in self.d:
+            return self.d[name]
+
+        if not url.endswith('/'):
+            url += '/'
+
+        # get information about this package from the Simple API in JSON
+        # format as per PEP 691
+        request = urllib.request.Request(
+            f"{url}{name}/",
+            headers={
+                "Accept": "application/vnd.pypi.simple.v1+json",
+            },
+        )
+
+        with urllib.request.urlopen(request) as response:
+            data = self.d[name] = json.load(response)
+            data['response_url'] = str(response.url)
+            return data
+
+
+RCACHE = RequestCache()
